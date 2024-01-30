e23309d12ad64bf4a55285073a7cbbbb27eb6121
Marcel <marceldjnairo@gmail.com>
2024-01-30 19:36:59 +0100

    Add ProcessUnansweredCommentsCommand

---
+++ app/Console/Commands/ProcessUnansweredCommentsCommand.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\CreateTldrCommentJob;
+use App\Models\Message;
+use Illuminate\Console\Command;
+
+class ProcessUnansweredCommentsCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'pr0:process-old-comments';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Tries to process old comments that were not answered yet.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): int
+    {
+        $this->info('Processing old comments...');
+
+        $comments = \App\Models\Message::where('tldrValue', null)->where('created_at', '>', now())->get();
+
+        $this->info('Found ' . count($comments) . ' comments.');
+        $this->info('Dispatching jobs...');
+        $this->withProgressBar($comments, function (Message $comment) {
+            CreateTldrCommentJob::dispatch($comment);
+        });
+
+        $this->info("\nDone!");
+
+        return Command::SUCCESS;
+    }
+}
